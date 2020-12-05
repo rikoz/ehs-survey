b73cb82609b1a5e1affbf2ad39e18885a2f6e3e2
rikoz <rikoz4intergrity@gmail.com>
2020-12-05 15:31:12 +0100

    - add survey description from DB
    - remove patient key from survey answer
    
    - radio template loop

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ var app = new Vue({
           this.answers = this.survey.data.questions.map(question => {
             return {
               question: question.id,
-              text: question.answer_type == 'Multiselect' ? [] : (question.answer_type == 'Rating' ? new Array(question.options.split('\r\n').length) : ''),
-              patient: '936825c1-2c15-43bb-9d7b-13d8d807857d'
+              text: question.answer_type == 'Multiselect' ? [] : (question.answer_type == 'Rating' ? new Array(question.options.split('\r\n').length) : '')
             }
           })
         })
@@ -54,8 +53,6 @@ var app = new Vue({
       if (e.target.checked) {
         theObj.text[optionId-1] = e.target.value
       }
-
-      console.log(theObj)
     },
     handleText (e) {
       const qid = e.srcElement.id
