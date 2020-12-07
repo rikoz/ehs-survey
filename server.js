1846a256e8783a2fb98dcdf8c13e8ae990a13931
rikoz <rikoz4intergrity@gmail.com>
2020-12-07 08:38:11 +0100

    use express.js for routing

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const open = require('open');
+
+const app = express();
+const port = 4050
+// var router = express.Router()
+
+app.engine('html', ejs.renderFile)
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/:surveyId/:userId', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.use(express.static(path.join(__dirname, '/assets')))
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`listening on port ${port}!`)
+    open(`http://localhost:${port}`);
+  }
+})
